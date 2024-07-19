cae841a69378c30a6cb4a1731b3c3d1f6987aee5
7006 <22399553+7006@users.noreply.github.com>
2024-07-19 16:45:35 +0000

    Poke around with wolfmq

--- src/vlm.erl
+++ src/vlm.erl
@@ -1,7 +1,23 @@
 -module(vlm).
 
 -export([start/0]).
+-export([enqueue_yolo/0]).
+-export([enqueue_frob/0]).
+-export([perform_task/1]).
 
 %% Used by make shell
 start() ->
     application:ensure_all_started(?MODULE).
+
+enqueue_yolo() ->
+    enqueue(yolo).
+
+enqueue_frob() ->
+    enqueue(frob).
+
+enqueue(QueueId) ->
+    ok = wolfmq:push(QueueId, {?MODULE, perform_task, [QueueId]}).
+
+perform_task(QueueId) ->
+    timer:sleep(5_000),
+    io:format("~s!~n", [QueueId]).
